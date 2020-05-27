f7dc1a4623b792316282b3dd44a6df2cd4688920
jcolinge <jacques.colinge@inserm.fr>
2020-05-27 10:26:30 +0200

    added top interations display

--- SDC-application.R
+++ SDC-application.R
@@ -21,9 +21,13 @@ stopCluster(cl)
 ds.LR <- getCorrelatedLR(ds,min.cor=0.25)
 ds.LR <- checkReceptorSignaling(ds,ds.LR)
 pp <- pValuesLR(ds.LR,ds$param)
+pp <- pp[order(pp$qval),]
+pp[1:20,c("L","R","pval","qval","pw.id","pw.name")] # top 20 interaction with pathway redundancy
 
 # extract gene signatures to report ligand-receptor-downstream pathway scores
 pp.red <- reduceToBestPathway(pp)
+pp.red[1:20,c("L","R","pval","qval","pw.id","pw.name")] # top 20 interactions without pathway redundancy
+sum(pp.red$qval<0.01) # number of significant interactions
 p.red.P <- reduceToPathway(pp.red)
 signatures <- getLRGeneSignatures(p.red.P,qval.thres=1e-4)
 scores <- scoreLRGeneSignatures(ds,signatures,rename.by.pathway=T)
